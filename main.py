4b791eb343f8f0edb7add914b2edaa8ced397f83
Raven <sd5356@rit.edu>
2025-03-01 18:45:03 -0500

    calculate and display pointing direction.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 import mediapipe as mp
-
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+import mediapipe.python.solutions.drawing_utils as mp_drawing
+import mediapipe.python.solutions.hands as mp_hands
+import numpy as np
 
 hands = mp_hands.Hands(
     static_image_mode=False,
@@ -26,31 +26,30 @@ while cap.isOpened():
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            # Knuckle landmark indices:
-            # 5: Wrist to Thumb knuckle
-            # 9: Wrist to Index finger knuckle
-            # 13: Wrist to Middle finger knuckle
-            # 17: Wrist to Ring finger knuckle
-            # 21: Wrist to Pinky finger knuckle
+    my_text = ""
 
-            knuckle_indices = [5, 9, 13, 17, 21]
-            knuckle_coordinates = []
+    if results.multi_hand_world_landmarks:
+        for hand_landmarks in results.multi_hand_world_landmarks:
+            index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+            index_tip = np.array((index_tip.x, index_tip.y, index_tip.z))
+            index_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]
+            index_mcp = np.array((index_mcp.x, index_mcp.y, index_mcp.z))
+            direction = index_tip - index_mcp
 
-            for index in knuckle_indices:
-                landmark = hand_landmarks.landmark[0]
-                h, w, c = image.shape
-                cx, cy = int(landmark.x * w), int(landmark.y * h)
-                knuckle_coordinates.append((landmark.x, landmark.y, landmark.z))
-                cv2.circle(
-                    image, (cx, cy), 5, (255, 0, 0), -1
-                )  # draw circles on knuckles.
+            x_dir = "left" if direction[0] < 0 else "right"
+            y_dir = "up" if direction[1] < 0 else "down"
+            z_dir = "forward" if direction[2] < 0 else "backward"
 
             # You can now use knuckle_coordinates (list of tuples) for further processing
-            print("Knuckle Coordinates:", knuckle_coordinates)
+            print(index_tip, index_mcp)
 
-            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            my_text = f"{x_dir}; {y_dir}; {z_dir}"
+            print(my_text)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    image = cv2.putText(
+        image, my_text, (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA
+    )
 
     cv2.imshow("MediaPipe Hands", image)
     if cv2.waitKey(5) & 0xFF == 27:
