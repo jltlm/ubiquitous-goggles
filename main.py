78590f15e072dce4fe5b638a0f542d8628e2c604
Raven <sd5356@rit.edu>
2025-03-01 19:07:11 -0500

    add: line drawing

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import cv2
-import mediapipe as mp
+import numpy as np
+import mediapipe.python.solutions.drawing_utils as mp_drawing
+import mediapipe.python.solutions.hands as mp_hands
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
 
 hands = mp_hands.Hands(
     static_image_mode=False,
-    max_num_hands=2,
+    max_num_hands=1,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5,
 )
@@ -25,32 +25,24 @@ while cap.isOpened():
 
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    h, w, c = image.shape
 
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            # Knuckle landmark indices:
-            # 5: Wrist to Thumb knuckle
-            # 9: Wrist to Index finger knuckle
-            # 13: Wrist to Middle finger knuckle
-            # 17: Wrist to Ring finger knuckle
-            # 21: Wrist to Pinky finger knuckle
-
-            knuckle_indices = [5, 9, 13, 17, 21]
-            knuckle_coordinates = []
-
-            for index in knuckle_indices:
-                landmark = hand_landmarks.landmark[0]
-                h, w, c = image.shape
-                cx, cy = int(landmark.x * w), int(landmark.y * h)
-                knuckle_coordinates.append((landmark.x, landmark.y, landmark.z))
-                cv2.circle(
-                    image, (cx, cy), 5, (255, 0, 0), -1
-                )  # draw circles on knuckles.
-
-            # You can now use knuckle_coordinates (list of tuples) for further processing
-            print("Knuckle Coordinates:", knuckle_coordinates)
-
             mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+            index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+            index_tip = np.array((int(index_tip.x * w), int(index_tip.y * h)))
+            index_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]
+            index_mcp = np.array((int(index_mcp.x * w), int(index_mcp.y * h)))
+            direction = index_tip - index_mcp
+            end = direction * 100 + index_tip
+
+            cv2.circle(image, (index_tip[0], index_tip[1]), 5, (255, 0, 0), -1)
+            cv2.circle(image, (index_mcp[0], index_mcp[1]), 5, (255, 0, 0), -1)
+            cv2.line(
+                image, (index_tip[0], index_tip[1]), (end[0], end[1]), (255, 0, 0), 5
+            )
 
     cv2.imshow("MediaPipe Hands", image)
     if cv2.waitKey(5) & 0xFF == 27:
