bb90fd2a050d7e80f72f57aae36e8d55572a09f0
Raven <sd5356@rit.edu>
2025-03-02 02:59:04 -0500

    update(#6): lower gesture recognizer confidence score.
    It was not recognizing thumb up with my left hand. Now it does.

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def process_results(result):
     if result.gestures:
         for gesture in result.gestures:
             print(gesture)
-            if gesture[0].category_name == "Thumb_Up" and gesture[0].score > 0.7:
+            if gesture[0].category_name == "Thumb_Up" and gesture[0].score > 0.6:
                 print("Thumbs_Up")
                 global game_is_on
                 game_is_on = True
